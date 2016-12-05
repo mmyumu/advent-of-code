4953f09c0ad70878ee4b366d4e5817cc754066d3
mmyumu <mmyumu@gmail.com>
2016-12-05 23:15:25 +0100

    Day5 - Part2: OK

--- 05.py
+++ 05.py
@@ -2,22 +2,27 @@ import md5
 
 doorId = "wtnhxymk"
 #doorId = "abc"
-password = ""
-
+password = ['-','-','-','-','-','-','-','-']
 
 index = 0
-for letter in doorId:
-	found = False
-	while found == False:
-		fullId = doorId + str(index)
-		myMd5 = md5.new(fullId)
+count = 0
+while count < 8:
+	fullId = doorId + str(index)
+	myMd5 = md5.new(fullId)
 
-		hexdigest = myMd5.hexdigest()
-		#print "Index: " + str(index) + " > Digest: " + hexdigest
-		if hexdigest.startswith("00000") == True:
-			print "Index: " + str(index) + ", hexdigest=" + hexdigest
-			password += hexdigest[5]
-			found = True
-		index += 1
+	hexdigest = myMd5.hexdigest()
+	#print "Index: " + str(index) + " > Digest: " + hexdigest
+	if hexdigest.startswith("00000") == True:
+		try:
+			position = int(hexdigest[5])
+			if position <= 7 and password[position] == '-':
+				print "Index: " + str(index) + ", hexdigest=" + hexdigest + " >> position=" + str(position)
+				password[position] = hexdigest[6]
+				print "Decrypting in progress [" + str(count) + "]: " + str(password)
+				count += 1
+		except:
+			print "Index: " + str(index) + ", position not valid: " + hexdigest[5]
 
-print password
+	index += 1
+
+print "Decrypting done: " + str(password)
