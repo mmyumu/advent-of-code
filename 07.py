20e8c5db172cb3e76651bd0649b61b7ead331922
mmyumu <mmyumu@gmail.com>
2016-12-08 01:19:46 +0100

    Day7 - Part2: OK

--- 07.py
+++ 07.py
@@ -11,23 +11,28 @@ elif(numberOfArgs == 2):
 f = open(inputFile, 'r')
 inputs = f.readlines()
 
-abbaPattern = "(?P<char1>\w)(?P<char2>(?!(?P=char1))\w)(?P=char2)(?P=char1)"
-#exclude = re.compile("\[[.^\]]*" + abbaPattern + "[.^\]]*\]")
-exclude = re.compile("\[[^\]]*"+abbaPattern+"[^\]]*\]")
-include = re.compile(abbaPattern)
+supernetPattern = "([^\[]*)(\[([^\]]*)\]|\n)"
+pattern = re.compile("(?P<char1>\w)(?P<char2>(?!(?P=char1))\w)(?P=char1).*----.*(?P=char2)(?P=char1)(?P=char2)")
+
 
 count = 0
 for input in inputs:
-	excluded = exclude.search(input)
-	if not excluded:
-		print input
-		included = include.search(input)
-		if included:
-			count += 1
-		#else:
-		#	print "not included=" + input
-	else:
-		print "excluded=" + input
+	if input[0] == "[":
+		raise NameError('Cannot parse line starting with [')
 
+	supernet = ""
+	hypernet = ""
+	for m in re.finditer(supernetPattern, input):
+		if m.group(1):
+			supernet += str(m.group(1)) + "-"
+		if m.group(3):
+			hypernet += str(m.group(3)) + "-"
+
+	line = supernet + "---" + hypernet
+
+	match = pattern.search(line)
+
+	if match:
+		count += 1
 
 print "Number of IPs: " + str(count)
